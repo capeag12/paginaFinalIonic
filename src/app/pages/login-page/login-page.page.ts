9ab2b97324733a3f1eca58b43155e2cf31930eea
Carlos Pérez Aguilera <capeag12@gmail.com>
2023-02-10 14:41:17 +0100

    V1

--- src/app/pages/login-page/login-page.page.ts
+++ src/app/pages/login-page/login-page.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.page.scss'],
 })
 export class LoginPagePage implements OnInit {
+  @ViewChild(IonModal) modal!:IonModal;
+  formRegistrarme:FormGroup;
 
-  constructor() { }
+  //Controles registrarme
+  nameRegistrarme:FormControl
+  
+  constructor() { 
+    this.formRegistrarme = new FormGroup([])
+    this.nameRegistrarme = new FormControl("",[Validators.required])
+  }
+
+  
 
   ngOnInit() {
+  }
+
+  cancelar(){
+    console.log("Cancelado")
+    this.modal.dismiss(null,"cancelar")
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    
   }
 
 }
